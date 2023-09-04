477e07fd2d2c5942b7c78c94efbf9b91979655a8
kseble <seble.temesgenk@gmail.com>
2023-09-04 08:32:08 -0700

    add

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,19 @@
+
+class Rectangle:
+    def __init__(self, left_upper: tuple, right_lower: tuple):
+        self.left_upper = left_upper
+        self.right_lower = right_lower
+        self.width = right_lower[0]-left_upper[0]
+        self.height = right_lower[1]-left_upper[1]
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return self.width * 2 + self.height * 2
+
+    def move(self, x_change: int, y_change: int):
+        corner = self.left_upper
+        self.left_upper = (corner[0]+x_change, corner[1]+y_change)
+        corner = self.right_lower
+        self.right_lower = (corner[0]+x_change, corner[1]+y_change)
