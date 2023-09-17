cffe964b47fd874883975fb2ebdaa1d237d5b05a
kseble <seble.temesgenk@gmail.com>
2023-09-17 10:26:30 -0700

    update

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define a class Base """
+
+
+class Base(__nb_objects):
+    """ """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            return __nb_objects = __nb_objects + 1
+        if id is not None:
+            return
