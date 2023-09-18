558c73420b7b0e5d67a87f4da54ea025178787ce
kseble <seble.temesgenk@gmail.com>
2023-09-18 12:32:20 -0700

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """define a class Base """
+import json
+import csv
+import turtle
 
 
-class Base(__nb_objects):
+class Base:
     """ """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is None:
-            return __nb_objects = __nb_objects + 1
         if id is not None:
-            return
+             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
