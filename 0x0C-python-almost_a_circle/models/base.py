d3974134eb0648e6ea5a75264ac947213d6f4eff
kseble <seble.temesgenk@gmail.com>
2023-09-19 11:17:34 -0700

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,20 @@
-#!/usr/bin/python
-"""define a class Base model. """
+#!/usr/bin/python3
+
+"""Defines a base model class."""
 import json
 import csv
 import turtle
 
 
 class Base:
-    """A class Base is defined.
+    """Base model.
+
+    This Represents the "base" for all other classes in project 0x0C*.
 
     Private Class Attributes:
         __nb_object (int): Number of instantiated Bases.
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
