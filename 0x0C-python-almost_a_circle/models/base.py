29e4a99ce3bfcfaabe5b1813936f7a7d0ca2a1f8
kseble <seble.temesgenk@gmail.com>
2023-09-19 08:42:57 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,27 @@
-#!/usr/bin/python3
-"""define a class Base """
+#!/usr/bin/python
+
+"""define a class Base model. """
 import json
 import csv
 import turtle
 
 
 class Base:
-    """ """
+    """A class Base is defined.
 
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
         if id is not None:
-             self.id = id
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
