938825c368934bbc1115228b5ce9b672562a95b4
kseble <seble.temesgenk@gmail.com>
2023-11-14 11:09:53 -0800

    edit

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-// a class Rectangle that defines Rectangle
-
+// class Rectangle that defines a rectangle
 class Rectangle {
-	constructor(w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
+
 module.exports = Rectangle;
